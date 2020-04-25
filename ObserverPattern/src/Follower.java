b8e060cea79833365c642f60dfa82ea77411cd18
junni <junni@LAPTOP-7OFC0AVT>
2020-04-24 19:36:04 -0500

    first commit

---
+++ ObserverPattern/src/Follower.java
@@ -0,0 +1,29 @@
+
+public class Follower implements Observer {
+private String followerName;
+	
+	public Follower(String follerName) {
+		this.followerName = follerName;
+		
+	}
+	
+	public String getFollowerName() {
+		return this.followerName;
+	}
+	
+	public void setFollowername(String followerName) {
+		 this.followerName = followerName;
+	}
+	
+	
+
+	public void update(String status) {
+		System.out.printf("%s received update\"%s\"\n", followerName, status);
+		
+	}
+	
+	public void play() {
+		System.out.println("Playing..... Video");
+	}
+	
+}
